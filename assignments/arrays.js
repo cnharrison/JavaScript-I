d52dad3d7a1c096fa9a2dbae0226f3e676246be7
Christopher Harrison <cnharrison@gmail.com>
2019-01-16 23:51:48 -0500

    woops

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -77,11 +77,11 @@ let inventory = [
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 console.log(
   `Car 33 is a ` +
-    inventory[33].car_year +
+    inventory[32].car_year +
     ` ` +
-    inventory[33].car_make +
+    inventory[32].car_make +
     ` ` +
-    inventory[33].car_model
+    inventory[32].car_model
 );
 
 // ==== Challenge 2 ====
